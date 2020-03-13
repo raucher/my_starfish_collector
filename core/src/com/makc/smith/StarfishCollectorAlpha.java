1bd84fa5a18bca1bee82ad539b6f68f2b3428fa9
raucher <raucher.devel@gmail.com>
2020-03-13 10:22:00 +0100

    adds textures

--- core/src/com/makc/smith/StarfishCollectorAlpha.java
+++ core/src/com/makc/smith/StarfishCollectorAlpha.java
@@ -1,33 +1,69 @@
 package com.makc.smith;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
-public class StarfishCollectorAlpha extends ApplicationAdapter {
+
+public class StarfishCollectorAlpha extends Game {
     SpriteBatch batch;
-    Texture img;
+
+    Texture oceanTexture;
+    Texture winMessageTexture;
+
+    boolean win = false;
+
+    // TURTLE
+    Texture turtleTexture;
+    Rectangle turtleRectangle;
+    float turtleX;
+    float turtleY;
+
+    // STARFISH
+    Texture starfishTexture;
+    Rectangle starfishRectangle;
+    float starfishX;
+    float starfishY;
 
     @Override
     public void create () {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+
+        turtleTexture = new Texture("turtle-1.png");
+        turtleX = 20;
+        turtleY = 20;
+        turtleRectangle =
+                new Rectangle(turtleX, turtleY, turtleTexture.getWidth(), turtleTexture.getHeight());
+
+        starfishTexture = new Texture("starfish.png");
+        starfishX = 320;
+        starfishY = 320;
+        starfishRectangle =
+                new Rectangle(starfishX, starfishY, starfishTexture.getWidth(), starfishTexture.getHeight());
+
+        oceanTexture = new Texture("water.jpg");
+        winMessageTexture = new Texture("you-win.png");
     }
 
     @Override
     public void render () {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0.1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, 0, 0);
+
+        batch.draw(turtleTexture, turtleX, turtleY);
+        batch.draw(starfishTexture, starfishX, starfishY);
+
         batch.end();
     }
 
     @Override
     public void dispose () {
         batch.dispose();
-        img.dispose();
     }
 }
