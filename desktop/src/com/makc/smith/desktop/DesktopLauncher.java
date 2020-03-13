9071e0933ed255e429df19ef5faebfc0c9728797
raucher <raucher.devel@gmail.com>
2020-03-13 09:57:43 +0100

    index

---
+++ desktop/src/com/makc/smith/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.makc.smith.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.makc.smith.StarfishCollectorAlpha;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarfishCollectorAlpha(), config);
+	}
+}
