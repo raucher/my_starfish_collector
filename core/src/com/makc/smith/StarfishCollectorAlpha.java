9af9ba20c1d6dd22bb213ca64096b41ee53e8de6
raucher <raucher.devel@gmail.com>
2020-03-13 10:48:43 +0100

    process user input

--- core/src/com/makc/smith/StarfishCollectorAlpha.java
+++ core/src/com/makc/smith/StarfishCollectorAlpha.java
@@ -1,11 +1,10 @@
 package com.makc.smith;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
@@ -23,6 +22,7 @@ public class StarfishCollectorAlpha extends Game {
     Rectangle turtleRectangle;
     float turtleX;
     float turtleY;
+    int turtleSpeed = 3;
 
     // STARFISH
     Texture starfishTexture;
@@ -52,18 +52,65 @@ public class StarfishCollectorAlpha extends Game {
 
     @Override
     public void render () {
+
+        // USER INPUT HANDLING
+        processUserInput();
+
+        // UPDATE GAME STATE
+        updateGameState();
+
+        // RENDERINIG //
         Gdx.gl.glClearColor(0, 0, 0.1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         batch.begin();
 
+        // OCEAN
+        batch.draw(oceanTexture, 0 ,0);
+
+        // STARFISH
+        if (!win) {
+            batch.draw(starfishTexture, starfishX, starfishY);
+        } else {
+            batch.draw(winMessageTexture, 175, 300);
+        }
+
+        // TURTLE
         batch.draw(turtleTexture, turtleX, turtleY);
-        batch.draw(starfishTexture, starfishX, starfishY);
 
         batch.end();
+    }
+
+    private void updateGameState() {
+        turtleRectangle.setPosition(turtleX, turtleY);
+
+        if (turtleRectangle.overlaps(starfishRectangle)) {
+            win = true;
+        }
+
+    }
+
+    private void processUserInput() {
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            turtleX -= turtleSpeed;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            turtleX += turtleSpeed;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)){
+            turtleY += turtleSpeed;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            turtleY -= turtleSpeed;
+        }
     }
 
     @Override
     public void dispose () {
         batch.dispose();
+        turtleTexture.dispose();
+        starfishTexture.dispose();
+        oceanTexture.dispose();
     }
 }
